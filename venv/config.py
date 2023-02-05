ab7ba6e888f974ea3f009c7f9ee57f7a818c2c84
Vera Lobova <pchilli@yandex.ru>
2023-02-05 17:06:48 +0300

    test

--- venv/config.py
+++ venv/config.py
@@ -1,4 +1,7 @@
 TOKEN_API = '6184979443:AAGtazwrRSDOCZTxKSalScAs4c86RUkUe3E'
 
-total = []
+global players_dic
+players_dic = {}
+global players_switch_dic
+players_switch_dic = {}
 player_switch = True
--- venv/handlers/take.py
+++ venv/handlers/take.py
@@ -6,7 +6,7 @@ from aiogram.types import ReplyKeyboardRemove
 from keyboards import spm_kb
 from keyboards import bot_game_kb
 from keyboards import kb
-from config import total, player_switch
+from config import players_dic, players_switch_dic
 
 
 
@@ -18,25 +18,28 @@ async def start_candy_game(message:Message):
  
 @dp.message_handler(commands = ['bot_candy_game'])
 async def bot_candy_game(message:Message):
-    global total
-    global player_switch
+    global set_number
+    
+   
+    global players_dic
+    players_dic[message.from_user.id] = set_number
+    global players_switch_dic
+    players_switch_dic[message.from_user.id] = randint(0,2)
 
-    total = int(randint(30, 101))
-    player_switch = randint(0,2)
 
-    if player_switch:
-        await message.answer(f'Кидаем жребий. Певым ходит {message.from_user.first_name}. На столе лежит {total} конфет, сколько берешь?')
+    if players_switch_dic[message.from_user.id] :
+        await message.answer(f'Кидаем жребий. Певым ходит {message.from_user.first_name}. На столе лежит {players_dic[message.from_user.id]} конфет, сколько берешь?')
     else:
         await message.answer(f'Кидаем жребий. Певым хожу я! Трепещи, кусок мяса!')
-        candy_bot_num = total%29
-        await message.answer(f"Мой ход! На столе {total} конфет! Я беру {candy_bot_num} конфет.")
-        total -= candy_bot_num
-        player_switch = True
-        if total <=0:
-            await winner_time()
+        candy_bot_num = players_dic[message.from_user.id]%29
+        await message.answer(f"Мой ход! На столе {players_dic[message.from_user.id]} конфет! Я беру {candy_bot_num} конфет.")
+        players_dic[message.from_user.id] -= candy_bot_num
+        players_switch_dic[message.from_user.id]  = True
+        if players_dic[message.from_user.id] <=0:
+            await winner_time(message.from_user.id)
             return True
         else:
-            await message.answer(f" На столе {total} конфет! Сколько возьмешь? ")
+            await message.answer(f" На столе {players_dic[message.from_user.id]} конфет! Сколько возьмешь? ")
         
 
 
@@ -44,50 +47,59 @@ async def bot_candy_game(message:Message):
         
 @dp.message_handler()
 async def candy_game(message:Message):
-    global player_switch
-    global total
-    global userId
-    userId = message.from_user.id
+    global players_dic
+    global players_switch_dic
+    
     count = int(message.text)
     name = message.from_user.first_name
     if message.text.isdigit() and 0 < count <29:
-    
-            total -= count
-            player_switch = False
-            await message.answer(f" {name} взял {count} конфет и на столе осталось {total}")
+            players_dic[message.from_user.id] -= count 
+            players_switch_dic[message.from_user.id] = False
+            await message.answer(f" {name} взял {count} конфет и на столе осталось {players_dic[message.from_user.id]}")
 
-            if total <= 0:
-                await winner_time()
+            if players_dic[message.from_user.id] <= 0:
+                await winner_time(message.from_user.id)
                 return True
             
             else:   # ходит бот
-                if total <= 28:
-                    candy_bot_num = total
-                elif total%29 == 0:
+                if players_dic[message.from_user.id] <= 28:
+                    candy_bot_num = players_dic[message.from_user.id]
+                elif players_dic[message.from_user.id]%29 == 0:
                     await message.answer("Я и не знал, что люди могут быть такие умные! Хотя, может это случайность...")
                     await bot.send_sticker(message.from_user.id, sticker ="CAACAgIAAxkBAAEHiqNj2l0YzrSBT5H9jaCsTMOSszikiwACGAADwDZPE9b6J7-cahj4LgQ")
                     candy_bot_num = randint(1, 28)
         
                 else:
-                    candy_bot_num = total%29
+                    candy_bot_num = players_dic[message.from_user.id]%29
             
-                await message.answer(f"Мой ход! На столе {total} конфет! Я беру {candy_bot_num} конфет.")
-                total -= candy_bot_num
-                player_switch = True
-            if total <=0:
-                await winner_time()
+                await message.answer(f"Мой ход! На столе {players_dic[message.from_user.id]} конфет! Я беру {candy_bot_num} конфет.")
+                players_dic[message.from_user.id] -= candy_bot_num
+                players_switch_dic[message.from_user.id]  = True
+            if players_dic[message.from_user.id] <=0:
+                await winner_time(message.from_user.id)
                 return True
             else:
-                await message.answer(f" На столе {total} конфет! Сколько возьмешь? ")
+                await message.answer(f" На столе {players_dic[message.from_user.id]} конфет! Сколько возьмешь? ")
+    
+    elif message.text.isdigit() and int(message.text)>28:
+        await message.answer("Ой. Можно взять только от 1 до 28 конфет.")
+    try:
+        int(message.text.isdigit)
+    except ValueError:
+        await message.answer(f"Простите, я еще не знаю, что такое '{message.text}'. Я еще совсем маленький бот. ")
 
 
 
 
-async def winner_time():
-    global userId
-    if player_switch:
+
+async def winner_time(userId):
+    
+    if players_switch_dic[userId]:
         await bot.send_message(chat_id=userId, text = "Я победил! Очевидно искуственный разум совершеннее человеческого!", reply_markup= kb)
         
     else:
         await bot.send_message(chat_id=userId, text ="Ты победил? 😭 Не может такого быть! Давай еще раз!\n ", reply_markup=bot_game_kb)
+
+
+
     
